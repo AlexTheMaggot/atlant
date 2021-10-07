2f06294afd7453cb664081cd992589ac4eb66896
alexthemaggot23@gmail.com <PN7V4czz25JVneD>
2021-10-08 01:23:35 +0500

    atlant v0.3 add product to main page add product_detail

--- atlant/mainapp/static/js/main.js
+++ atlant/mainapp/static/js/main.js
@@ -3,13 +3,17 @@ $(window).on('load', function () {
         let path, new_path;
         path = window.location.pathname;
         new_path = path.replace('/uz/', '');
-        $('#header_lang_uz').attr('href', path);
-        $('#header_lang_ru').attr('href', '/' + new_path);
+        $('#header_lang_uz').attr('href', path + window.location.search);
+        $('#mob_header_lang_uz').attr('href', path + window.location.search);
+        $('#header_lang_ru').attr('href', '/' + new_path + window.location.search);
+        $('#mob_header_lang_ru').attr('href', '/' + new_path + window.location.search);
     }
     else {
         let path;
         path = window.location.pathname;
-        $('#header_lang_ru').attr('href', path);
-        $('#header_lang_uz').attr('href', '/uz' + path);
+        $('#header_lang_ru').attr('href', path + window.location.search);
+        $('#header_lang_uz').attr('href', '/uz' + path + window.location.search);
+        $('#mob_header_lang_ru').attr('href', path + window.location.search);
+        $('#mob_header_lang_uz').attr('href', '/uz' + path + window.location.search);
     }
 });
