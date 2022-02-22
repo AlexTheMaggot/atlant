e2f238c2abe3edfdb59dc3b2ff965af2f782907a
alexthemaggot23@gmail.com <PN7V4czz25JVneD>
2022-02-22 23:34:35 +0500

    fix search url

--- atlant/shop/urls_uz.py
+++ atlant/shop/urls_uz.py
@@ -5,7 +5,7 @@ app_name = 'catalog_uz'
 
 urlpatterns = [
     path('', views.product_list, name='product_list'),
+    path('search/', views.search, name='search'),
     path('<slug:category_slug>/', views.category_detail, name='category_detail'),
     path('<slug:category_slug>/<slug:product_slug>', views.product_detail, name='product_detail'),
-    path('search', views.search, name='search')
 ]
