3f153c16a1360fedbd92ac27d46bf80d46f15344
alexagapkin <alexthemaggot23@gmail.com>
2022-06-11 17:25:13 +0500

    add sending mail views and thank_you view

--- atlant/mainapp/views.py
+++ atlant/mainapp/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.core.paginator import Paginator
+from django.core.mail import send_mail
+
 from . import models
 from shop.models import Category, Product, RentCategory, RentProduct
+
+
+MAIL_TO = 'atlantgroups1@gmail.com'
 
 
 def get_context():
@@ -113,3 +118,146 @@ def custom_404(request, exception):
     template = 'custom_404.html'
     return render(request, template, context)
 
+
+def callback_send(request):
+    if request.method == "POST":
+        subject = 'Новая заявка!'
+        message = 'Имя: {0}\nТелефон: {1}'.format(request.POST['name'], request.POST['phone'])
+        if request.POST['quantity']:
+            message += '\nКоличество: ' + str(request.POST['quantity'])
+        if request.POST['time']:
+            message += '\nСрок: ' + str(request.POST['time'])
+        if request.POST['text']:
+            message += '\n\nСообщение: ' + str(request.POST['text'])
+        send_mail(subject, message, 'no-reply@atlant-group.uz', [MAIL_TO])
+        if request.POST['lang'] == 'uz':
+            return redirect('mainapp_uz:thank_you')
+        else:
+            return redirect('mainapp:thank_you')
+    else:
+        return redirect('mainapp:index')
+
+
+def another_callback_send(request):
+    if request.method == "POST":
+        subject = 'Новая заявка!'
+        message = 'Имя: {0}\nТелефон: {1}'.format(request.POST['name'], request.POST['phone'])
+        if request.POST['time_to_call']:
+            message += '\nУдобное время для звонка: ' + str(request.POST['time_to_call'])
+        if request.POST['text']:
+            message += '\n\nСообщение: ' + str(request.POST['text'])
+        send_mail(subject, message, 'no-reply@atlant-group.uz', [MAIL_TO])
+        if request.POST['lang'] == 'uz':
+            return redirect('mainapp_uz:thank_you')
+        else:
+            return redirect('mainapp:thank_you')
+    else:
+        return redirect('mainapp:index')
+
+
+def one_more_callback_send(request):
+    if request.method == "POST":
+        subject = 'Новая заявка!'
+        message = 'Имя: {0}\nТелефон: {1}'.format(request.POST['name'], request.POST['phone'])
+        if request.POST['text']:
+            message += '\n\nСообщение: ' + str(request.POST['text'])
+        send_mail(subject, message, 'no-reply@atlant-group.uz', [MAIL_TO])
+        if request.POST['lang'] == 'uz':
+            return redirect('mainapp_uz:thank_you')
+        else:
+            return redirect('mainapp:thank_you')
+    else:
+        return redirect('mainapp:index')
+
+
+def rent_order(request):
+    if request.method == "POST":
+        subject = 'Новая заявка!'
+        message = 'Имя: {0}\nТелефон: {1}\nПродукт: {2}'.format(
+            request.POST['name'],
+            request.POST['phone'],
+            request.POST['rent_product']
+        )
+        if request.POST['quantity']:
+            message += '\n\nКоличество: ' + str(request.POST['quantity'])
+        if request.POST['text']:
+            message += '\n\nСообщение: ' + str(request.POST['text'])
+        send_mail(subject, message, 'no-reply@atlant-group.uz', [MAIL_TO])
+        if request.POST['lang'] == 'uz':
+            return redirect('mainapp_uz:thank_you')
+        else:
+            return redirect('mainapp:thank_you')
+    else:
+        return redirect('mainapp:index')
+
+
+def not_found_send(request):
+    if request.method == "POST":
+        subject = 'Новая заявка!'
+        message = 'Имя: {0}\nТелефон: {1}'.format(
+            request.POST['name'],
+            request.POST['phone'],
+        )
+        if request.POST['time']:
+            message += '\n\nУдобное время для звонка: ' + str(request.POST['time'])
+        if request.POST['text']:
+            message += '\n\nСообщение: ' + str(request.POST['text'])
+        send_mail(subject, message, 'no-reply@atlant-group.uz', [MAIL_TO])
+        if request.POST['lang'] == 'uz':
+            return redirect('mainapp_uz:thank_you')
+        else:
+            return redirect('mainapp:thank_you')
+    else:
+        return redirect('mainapp:index')
+
+
+def product_order(request):
+    if request.method == "POST":
+        subject = 'Новая заявка!'
+        message = 'Имя: {0}\nТелефон: {1}'.format(
+            request.POST['name'],
+            request.POST['phone'],
+        )
+        if request.POST['type'] == 'buy':
+            message += '\n\nТип: Покупка'
+        elif request.POST['type'] == 'ask':
+            message += '\n\nТип: Узнать цену и условия'
+        else:
+            message += '\n\nТип: Аренда'
+            if request.POST['time']:
+                message += '\n\nСрок: ' + str(request.POST['time'])
+        if request.POST['quantity']:
+            message += '\n\nКоличество: ' + str(request.POST['quantity'])
+        if request.POST['text']:
+            message += '\n\nСообщение: ' + str(request.POST['text'])
+        send_mail(subject, message, 'no-reply@atlant-group.uz', [MAIL_TO])
+        if request.POST['lang'] == 'uz':
+            return redirect('mainapp_uz:thank_you')
+        else:
+            return redirect('mainapp:thank_you')
+    else:
+        return redirect('mainapp:index')
+
+
+def service_order(request):
+    if request.method == "POST":
+        subject = 'Новая заявка!'
+        message = 'Имя: {0}\nТелефон: {1}'.format(request.POST['name'], request.POST['phone'])
+        if request.POST['text']:
+            message += '\n\nСообщение: ' + str(request.POST['text'])
+        send_mail(subject, message, 'no-reply@atlant-group.uz', [MAIL_TO])
+        if request.POST['lang'] == 'uz':
+            return redirect('mainapp_uz:thank_you')
+        else:
+            return redirect('mainapp:thank_you')
+    else:
+        return redirect('mainapp:index')
+
+
+def thank_you(request):
+    context = get_context()
+    if '/uz/' in request.path:
+        template = 'mainapp/thank_you_uz.html'
+    else:
+        template = 'mainapp/thank_you.html'
+    return render(request, template, context)
