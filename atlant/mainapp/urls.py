5087bbbf653a4dc06d011bfcbcd6d18163f3dde5
alexagapkin <alexthemaggot23@gmail.com>
2022-06-11 17:26:10 +0500

    add mail send and thank_you urls

--- atlant/mainapp/urls.py
+++ atlant/mainapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from shop import views as shop_views
+
 
 app_name = 'mainapp'
 
@@ -11,4 +11,12 @@ urlpatterns = [
     path('contacts/', views.contacts, name='contacts'),
     path('delivery/', views.delivery, name='delivery'),
     path('services/', views.services, name='services'),
+    path('callback_send/', views.callback_send, name='callback_send'),
+    path('another_callback_send/', views.another_callback_send, name='another_callback_send'),
+    path('one_more_callback_send/', views.one_more_callback_send, name='one_more_callback_send'),
+    path('rent_order/', views.rent_order, name='rent_order'),
+    path('not_found_send/', views.not_found_send, name='not_found_send'),
+    path('product_order/', views.product_order, name='product_order'),
+    path('service_order/', views.service_order, name='service_order'),
+    path('thank-you/', views.thank_you, name='thank_you'),
 ]
